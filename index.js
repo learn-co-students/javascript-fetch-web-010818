73d70c7b8778c0d2def225a16c61fad0fd7ef879
tanelam <tania.e.aparicio@gmail.com>
2018-02-26 13:46:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const token = 'fc270c2359d04e54eebc51670f86fe4af365eeba'
+
+fetch('https://api.github.com/repos/jquery/jquery/commits', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json())
+  .then(json => console.log(json));
